a54877cdd0fdb85bc708957b6c8dfd257f17031e
Jinentonik <garoad91@gmail.com>
2020-04-14 17:22:07 +0800

    first deploy

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -13,9 +13,9 @@ from instagram_web.util.email import send_message
 gateway = braintree.BraintreeGateway(
     braintree.Configuration(
         braintree.Environment.Sandbox,
-        merchant_id=os.environ.get('MERCHANT_ID'),
-        public_key=os.environ.get('PUBLIC_KEY'),
-        private_key=os.environ.get('PRIVATE_KEY')
+        merchant_id=os.environ.get('BRAINTREE_MERCHANT_ID'),
+        public_key=os.environ.get('BRAINTREE_PUBLIC_KEY'),
+        private_key=os.environ.get('BRAINTREE_PRIVATE_KEY')
     )
 )
 
